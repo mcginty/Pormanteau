8961099a52eda9d8a9146f911ec2226f5acfce21
Jake McGinty <mcginty@illinois.edu>
2011-03-16 19:37:12 -0500

    first push, still missing CV libraries.

---
+++ src/com/cs498/portmanteau/Portmanteau.java
@@ -0,0 +1,13 @@
+package com.cs498.portmanteau;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Portmanteau extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
